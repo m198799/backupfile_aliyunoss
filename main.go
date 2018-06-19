4bfe56eb3db8c1d725b164b1c34c21654ecd3783
潘旭 <xu.pan@ishansong.com>
2018-06-19 17:30:58 +0800

    增加了并行

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"flag"
 	"github.com/pkevin0909/backupfile_aliyunoss/file"
 	"strconv"
-	"time"
+//	"time"
 	"strings"
 )
 
@@ -17,15 +17,62 @@ func handleError(err error) {
 	os.Exit(-1)
 }
 
-func file_exsit(file string){
+func file_exsit(file string)error{
 	_, err := os.Stat(file)
 	if os.IsNotExist(err){
-		fmt.Println("file is not exist")
-		os.Exit(-1)
+		fmt.Println("file is not exist:",file)
+		return err
 	}
 	fmt.Println("file path:",file)
+	return nil
 }
 
+func runSyncfiles (v string,bucket *oss.Bucket,config *goini.Config,ch chan int){
+
+
+	file.ListDir(v)
+
+	expirationTime := config.GetValue("oss","expirationTime")
+	expTime, err := strconv.ParseInt(expirationTime,10,64)
+	if err != nil{
+		fmt.Println("strconv false\n")
+		fmt.Println("Error:", err)
+		os.Exit(-1)
+	}
+
+	files, err := file.ListChangeDir(v, expTime)
+
+	for _, table1 := range files {
+		fmt.Println("table1:", table1)
+		var table2 string = string([]rune(table1)[1:])
+		fmt.Println("table2:", table2)
+		err = bucket.PutObjectFromFile(table2, table1)
+		if err != nil {
+			handleError(err)
+		}
+	}
+
+	delTimeStr := config.GetValue("oss", "delTime")
+	delTime, err := strconv.ParseInt(delTimeStr, 10, 64)
+
+	if err != nil {
+		fmt.Println("strconv false\n")
+		fmt.Println("Error:", err)
+		os.Exit(-1)
+	}
+
+	if delTime == 0 {
+		fmt.Println("do not delete files")
+	} else {
+		if delTime > expTime {
+			file.Delete(v, delTime)
+			fmt.Println("delete file:",v)
+		} else {
+			fmt.Println("delTime need > expirationTime")
+		}
+	}
+	ch <- 1
+}
 
 func main() {
 
@@ -50,10 +97,10 @@ func main() {
 		fmt.Println("bucket:", bucket.Name)
 	}
 
-		bucket, err := client.Bucket(config.GetValue("oss","bucketName"))
-		if err !=nil{
-			handleError(err)
-		}
+	bucket, err := client.Bucket(config.GetValue("oss","bucketName"))
+	if err !=nil{
+		handleError(err)
+	}
 	//	lsObs, err := bucket.ListObjects()
 
 	//	if err != nil{
@@ -72,8 +119,7 @@ func main() {
 	// if err != nil {
 	//    handleError(err)
 	// }
-	expirationTime := config.GetValue("oss","expirationTime")
-	expTime, err := strconv.ParseInt(expirationTime,10,64)
+
 	runTimeOne := config.GetValue("oss","runTime")
 	runTime, err := strconv.Atoi(runTimeOne)
 	if err != nil{
@@ -85,43 +131,18 @@ func main() {
 	if runTime <= 0 {
 		runTime = 1
 	}
-
+	chs := make([]chan int,10)
 	dirPath := config.GetValue("oss", "dirPath")
-	fmt.Println(dirPath)
-//	filess := map[int]string{}
-	for _, v := range strings.Split(dirPath, ",") {
 
-		file_exsit(v)
-		file.ListDir(v)
-		for {
-			files, err := file.ListChangeDir(v, expTime)
-
-			for _, table1 := range files {
-				fmt.Println("table1:", table1)
-				var table2 string = string([]rune(table1)[1:])
-				fmt.Println("table2:", table2)
-				err = bucket.PutObjectFromFile(table2, table1)
-				if err != nil {
-					handleError(err)
-				}
-			}
-			delTimeStr := config.GetValue("oss", "delTime")
-			delTime, err := strconv.ParseInt(delTimeStr, 10, 64)
-			if err != nil {
-				fmt.Println("strconv false\n")
-				fmt.Println("Error:", err)
-				os.Exit(-1)
-			}
-			if delTime == 0 {
-				fmt.Println("do not delete files")
-			} else {
-				if delTime > expTime {
-					file.Delete(v, delTime)
-				} else {
-					fmt.Println("delTime need > expirationTime")
-				}
-			}
-			time.Sleep(time.Hour * time.Duration(runTime))
+	for i, v := range strings.Split(dirPath, ",") {
+		err:=file_exsit(v)
+		if err != nil{
+			continue;
 		}
+		chs[i] = make(chan int)
+		go runSyncfiles(v,bucket,config,chs[i])
+	}
+	for _, ch := range(chs) {
+		<-ch
 	}
 }
